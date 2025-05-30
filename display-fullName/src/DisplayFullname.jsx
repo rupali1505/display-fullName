67816711f6556d8e1d73939736a00cf6e326b692
rupali dudhal <rupalidudhal95@gmail.com>
2025-05-30 16:10:23 +0530

    completed

---
+++ display-fullName/src/DisplayFullname.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+export default function DisplayName() {
+  const [displayForm, setDisplayForm] = useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  const [formData, setFormData] = useState("");
+
+  const handleChange = (e) => {
+    setDisplayForm({
+      ...displayForm,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitData = (e) => {
+    e.preventDefault();
+
+    setFormData(`${displayForm.firstName}, ${displayForm.lastName}`);
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          display: "block",
+        }}
+      >
+        <form onSubmit={submitData}>
+          <label htmlFor="">First Name:</label>
+
+          <input
+            type="text"
+            name="firstName"
+            value={displayForm.firstName}
+            onChange={handleChange}
+            required
+          />
+
+          <br />
+
+          <label>Last Name:</label>
+
+          <input
+            type="text"
+            name="lastName"
+            value={displayForm.lastName}
+            onChange={handleChange}
+            required
+          />
+
+          <br />
+
+          <button>Submit</button>
+        </form>
+        {formData ? (
+          <p>
+            Full Name: {displayForm.firstName} {displayForm.lastName}.
+          </p>
+        ) : null}
+      </div>
+    </>
+  );
+}
+
+
+
